8b8f739fb4b90b585ad46dadc66d89cda23217fc
Ajarmar <37733838+Ajarmar@users.noreply.github.com>
2018-06-20 22:20:15 +0200

    z3: can load stages, reload checkpoints

--- Z3/z3-stageselect.asm
+++ Z3/z3-stageselect.asm
@@ -1,5 +1,9 @@
     .gba
     
+    ; Stage select menu x position offsets
+    MENU_OFFSET equ 5
+    CURSOR_OFFSET equ 4
+    
     ; Change to existing code.
     ; Manually changes the pool for loading the base address
     ; for game state subroutines. (See below.)
@@ -41,7 +45,7 @@
     .dw 0x080F2F45 ;1B
     .dw 0x080F3045 ;1C
     .dw 0x080F3129 ;1D
-    .dw 0x08386E00 ;1E CHANGE TO WHERE STAGE SELECT MENU ROUTINE BEGINS
+    .dw 0x08386E01 ;1E CHANGE TO WHERE STAGE SELECT MENU ROUTINE BEGINS
     .endarea
     
     ; New code. Stage order for the stage select menu.
@@ -81,6 +85,7 @@
     .asciiz "BABY ELVES 2"
     .asciiz "FINAL"
     .asciiz "COMMANDER ROOM"
+    .asciiz ">"
     
     ; New code. Recreation of stage select menu routine from Z2.
     ; ORDER OF OPERATIONS:
@@ -95,19 +100,239 @@
     ; 8. End
     .org 0x08386E00
     push    {r4-r7,r14}
-    add     sp,-#0x14
+    sub     sp,#0x14
     mov     r6,r0
-    ldr     r0,=#0x7254
-    add     r5,r6,r0        ; r5 should be stage index value
+    ldr     r0,=#0x6260
+    add     r5,r6,r0        ; Fixed (r5 is stage index address)
     ldrb    r4,[r5]
     ldr     r1,=#0x08386C70 ; Fixed (stage indexes)
     mov     r0,r13
     mov     r2,#0x11
-    bl      check_a_bunch
+    bl      @indexes_on_stack
     ldr     r7,=#0x02001EB0 ; Fixed (input checking)
-    
-    
-check_a_bunch:
-    
-    
+    ldrh    r1,[r7,#0x6]
+@check_for_down:
+    mov     r0,#0x80
+    and     r0,r1
+    cmp     r0,#0x0
+    beq     @check_for_up
+    add     r0,r4,#0x1
+    lsl     r0,r0,#0x18
+    lsr     r4,r0,#0x18
+@check_for_up:
+    mov     r0,#0x40
+    and     r0,r1
+    cmp     r0,#0x0
+    beq     @no_up
+    mov     r0,r4
+    sub     r0,#0x1
+    lsl     r0,r0,#0x18
+    lsr     r4,r0,#0x18
+@no_up:
+    mov     r0,r4
+    cmp     r0,#0xFF
+    bne     @no_upwrap
+    mov     r0,#0x10
+@no_upwrap:
+    cmp     r0,#0x11
+    bne     @no_downwrap
+    mov     r0,#0x0
+@no_downwrap:
+    lsl     r0,r0,#0x18
+    lsr     r4,r0,#0x18
+    ldr     r0,=#0x08386C84 ; "INTRO"
+    mov     r1,MENU_OFFSET  ; x offset
+    mov     r2,#0x1         ; y offset
+    bl      @draw_textline
+    ldr     r0,=#0x08386C8A ; "HELLBAT SCHILT"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x2
+    bl      @draw_textline
+    ldr     r0,=#0x08386C99 ; "BLAZIN' FLIZARD"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x3
+    bl      @draw_textline
+    ldr     r0,=#0x08386CA9 ; "DEATHTANZ MANTISK"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x4
+    bl      @draw_textline
+    ldr     r0,=#0x08386CBB ; "CHILDRE INARABITTA"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x5
+    bl      @draw_textline
+    ldr     r0,=#0x08386CCE ; "BABY ELVES 1"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x6
+    bl      @draw_textline
+    ldr     r0,=#0x08386CDB ; "BLIZZACK STAGGROFF"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x7
+    bl      @draw_textline
+    ldr     r0,=#0x08386CEE ; "HANUMACHINE"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x8
+    bl      @draw_textline
+    ldr     r0,=#0x08386CFA ; "ANUBIS NECROMANCESS V"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x9
+    bl      @draw_textline
+    ldr     r0,=#0x08386D10 ; "COPY X MK 2"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xA
+    bl      @draw_textline
+    ldr     r0,=#0x08386D1C ; "GLACIER LE CACTANK"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xB
+    bl      @draw_textline
+    ldr     r0,=#0x08386D2F ; "CUBIT FOXTAR"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xC
+    bl      @draw_textline
+    ldr     r0,=#0x08386D3C ; "TRETISTA KELVERIAN"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xD
+    bl      @draw_textline
+    ldr     r0,=#0x08386D4F ; "VOLTEEL BIBLIO"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xE
+    bl      @draw_textline
+    ldr     r0,=#0x08386D5E ; "BABY ELVES 2"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0xF
+    bl      @draw_textline
+    ldr     r0,=#0x08386D6B ; "FINAL"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x10
+    bl      @draw_textline
+    ldr     r0,=#0x08386D71 ; "COMMANDER ROOM"
+    mov     r1,MENU_OFFSET
+    mov     r2,#0x11
+    bl      @draw_textline
+    ldr     r0,=#0x08386D80 ; ">"
+    mov     r1,CURSOR_OFFSET
+    add     r2,r4,#0x1
+    bl      @draw_textline
+    str     r4,[r5]
+    ldrh    r1,[r7,#0x4]    ; Check for A input
+    mov     r0,#0x1
+    and     r0,r1
+    cmp     r0,#0x0
+    beq     @@subr_end
+    mov     r1,r13
+    add     r0,r1,r4
+    ldrb    r0,[r0]
+    str     r0,[r5]
+    ldr     r2,=#0x0835780E ; FIX: should be base address for stage settings
+    mov     r1,r4
+    bl      @stage_settings
+    bl      0x08019E94      ; should be somewhere far away
+    mov     r1,#0xC0
+    lsl     r1,r1,#0x2
+    str     r1,[r6]
+    mov     r1,#0x0
+    str     r1,[r6,#0x4]
+    str     r1,[r6,#0x8]
+@@subr_end:
+    add     sp,#0x14
+    pop     {r4-r7}
+    pop     {r0}
+    bx      r0
     .pool
+    
+@indexes_on_stack: ; r0 = sp, r1 = stage index list, r2 = 0x11
+    push    {r4-r7,r14}
+    mov     r5,r0
+    mov     r4,r5
+    mov     r3,r1
+    cmp     r2,0xF          ; Probably unnecessary
+    bls     @@hmm
+    mov     r0,r3
+    orr     r0,r5
+    mov     r1,#0x3
+    and     r0,r1
+    cmp     r0,#0x0
+    bne     @@hmm
+    mov     r1,r5
+@@move_stuff:
+    ldmia   r3!,{r0,r6,r7}
+    stmia   r1!,{r0,r6,r7}
+    ldmia   r3!,{r0}
+    stmia   r1!,{r0}
+    sub     r2,#0x10
+    cmp     r2,#0xF         ; Probably unnecessary
+    bhi     @@move_stuff
+    cmp     r2,#0x3
+    bls     @@something
+@@move_a_word:
+    ldmia   r3!,{r0}
+    stmia   r1!,{r0}
+    sub     r2,#0x4
+    cmp     r2,#0x3
+    bhi     @@move_a_word
+@@something:
+    mov     r4,r1
+@@hmm:
+    sub     r2,#0x1
+    mov     r0,#0x1
+    neg     r0,r0
+    cmp     r2,r0
+    beq     @@subr_end
+    mov     r1,r0
+@@move_a_byte:
+    ldrb    r0,[r3]
+    strb    r0,[r4]
+    add     r3,#0x1
+    add     r4,#0x1
+    sub     r2,#0x1
+    cmp     r2,r1
+    bne     @@move_a_byte
+@@subr_end:
+    mov     r0,r5
+    pop     {r4-r7,r15}
+    
+@draw_textline:
+    push    {r4-r6,r14}
+    mov     r3,r0
+    mov     r4,r1
+    ldr     r1,=#0x02030B70 ; Fixed (BG0 tiles)
+    cmp     r2,#0x1F
+    bhi     @@subr_end      ; Avoid trying to draw outside of screen
+    lsl     r0,r2,#0x5
+    add     r0,r4,r0
+    lsl     r0,r0,#0x1
+    add     r1,r1,r0
+    ldrb    r0,[r3]
+    cmp     r0,#0x0         ; Check for terminating null byte
+    beq     @@subr_end      ; Go to end if there is a null byte
+    mov     r2,#0xE8
+    lsl     r2,r2,#0x2
+    mov     r5,r2
+    mov     r6,#0xB8
+    lsl     r6,r6,#0x2
+    mov     r2,r6
+@@write_loop:
+    cmp     r4,#0x1F
+    bhi     @@subr_end      ; Avoid trying to draw outside of screen again
+    cmp     r0,#0x9F
+    bhi     @@not_sure      ; I don't think this branch will ever be used, but who knows
+    ldrb    r6,[r3]
+    add     r0,r5,r6
+    b       @@store_letter
+@@not_sure:
+    ldrb    r6,[r3]
+    add     r0,r2,r6
+@@store_letter:
+    strh    r0,[r1]
+    add     r3,#0x1
+    add     r1,#0x2
+    ldrb    r0,[r3]
+    cmp     r0,#0x0         ; Check for terminating null byte
+    bne     @@write_loop
+@@subr_end:
+    pop     {r4-r6}
+    pop     {r0}
+    bx      r0
+    .pool
+    
+@stage_settings:
+    bx      r14
--- Z3/z3prac.asm
+++ Z3/z3prac.asm
@@ -2,4 +2,97 @@
     .open "Rockman Zero 3 (Japan).gba", "z3prac.gba", 0x08000000
     .include "z3-stageselect.asm"
     
+    .org 0x080019D0
+    bl      0x08387100
+    
+    .org 0x08387100
+    push    {r4-r7,r14}
+    mov     r7,r10
+    mov     r6,r9
+    mov     r5,r8
+    push    {r5-r7}
+    ldr     r0,=#0x02001EB0
+    ldrh    r1,[r0]
+    mov     r2,#0x4
+    and     r2,r1
+    cmp     r2,#0x0
+    bne     @select_pressed
+    mov     r2,#0x80
+    lsl     r2,r2,#0x1
+    and     r2,r1
+    cmp     r2,#0x0
+    bne     @r_pressed
+    b       @subr_end
+@select_pressed:
+    ldrh    r1,[r0,#0x4]
+    mov     r2,#0x80
+    lsl     r3,r2,#0x2
+    and     r3,r1
+    cmp     r3,#0x0
+    bne     @load_checkpoint           ; Change to load state subroutine
+    lsl     r3,r2,#0x1
+    and     r3,r1
+    cmp     r3,#0x0
+    bne     @subr_end                  ; Change to save state subroutine
+    mov     r2,#0x8
+    and     r2,r1
+    cmp     r2,#0x0
+    beq     @subr_end
+    bl      @check_state
+    ldr     r4,=#0x02030B61
+    mov     r5,#0x1E
+    strb    r5,[r4]
+    b       @subr_end
+@r_pressed:
+    ldrh    r1,[r0,#0x4]
+    mov     r2,#0x4
+    and     r2,r1
+    cmp     r2,#0x0
+    bne     @subr_end                  ; Change to save state subroutine
+@subr_end:
+    pop     {r5-r7}
+    mov     r8,r5
+    mov     r9,r6
+    mov     r10,r7
+    pop     {r4-r7}
+    mov     r2,#0x80
+    lsl     r2,r2,#0x13
+    pop     r0
+    bx      r0
+    
+@load_checkpoint:
+    bl      @check_state
+    ldr     r4,=#0x02030B61
+    mov     r5,#0x3
+    strb    r5,[r4]
+    ; ldr     r1,=#0x02036C10     ; "Saved gameplay settings" section to write to
+    ; ldr     r0,=#0x02037EDC     ; Read from control settings
+    ; ldmia   r0!,{r2-r7}         ; Load 24 bytes
+    ; stmia   r1!,{r2-r7}         ; Store 24 bytes
+    ; ldmia   r0!,{r2-r7}         ; Load 24 bytes
+    ; stmia   r1!,{r2-r7}         ; Store 24 bytes
+    ; ldmia   r0!,{r2-r7}         ; Load 24 bytes
+    ; stmia   r1!,{r2-r7}         ; Store 24 bytes
+    ; ldmia   r0!,{r2-r3}         ; Load 8 bytes
+    ; stmia   r1!,{r2-r3}         ; Store 8 bytes
+    ; ldrh    r2,[r0]             ; Load another 2 bytes
+    ; strh    r2,[r1]             ; Store another 2 bytes
+    b       @subr_end
+    
+    ; Jumps to subroutine end if the current game state is 0, 1 or 2
+    ; Used to prevent certain functionality from being used while 
+    ; the game is paused.
+@check_state:
+    ldr     r4,=#0x02030B61
+    ldrb    r4,[r4]
+    cmp     r4,#0x0
+    beq     @subr_end
+    cmp     r4,#0x1
+    beq     @subr_end
+    cmp     r4,#0x2
+    beq     @subr_end
+    bx      r14
+    
+    .pool
+    
     .close
