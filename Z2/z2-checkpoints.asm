8b10ec38ae297e44b9f8e8e94a69a49f49413712
Ajarmar <37733838+Ajarmar@users.noreply.github.com>
2021-01-16 16:52:03 +0100

    z2: add platinum support for checkpoints

--- Z2/z2-checkpoints.asm
+++ Z2/z2-checkpoints.asm
@@ -2,7 +2,7 @@
     .org REG_CHECKPOINTS
     .area REG_CHECKPOINTS_AREA
 load_checkpoint:
-    push    r14
+    push    {r4-r7,r14}
     ldr     r4,=#ADDR_STAGETIME             ; Stage timer
     mov     r5,#0x0
     str     r5,[r4]
@@ -25,21 +25,20 @@ load_checkpoint:
     ldr     r1,=#ADDR_STORED_ZERO_DATA
     bl      ROMADDR_STORE_ZERO_DATA
 @after_settings:
-    ldr     r3,=#ADDR_STAGE_INDEX
-    ldrb    r4,[r3]
-    mov     r0,r4
-    cmp     r4,#0x11
+    ldr     r4,=#ADDR_STAGE_INDEX
+    ldrb    r3,[r4]
+    cmp     r3,#0x11
     beq     @checkpoints_end
-    sub     r4,r4,1
-    mov     r5,#0x10
-    mul     r4,r5
-    ldr     r6,=#org(@checkpoint_indices_base)
-    add     r6,r6,r4
-    ldrb    r1,[r3,#0x2]
+    sub     r3,r3,1
     mov     r2,r3
-;    b       @check_if_boss
+    lsl     r3,#0x4
+    ldr     r6,=#org(@checkpoint_indices_base)
+    add     r6,r6,r3
+    ldrb    r0,[r4,#0x2]
+    mov     r1,r4
+    bl      @check_if_boss
 @after_boss_check:
-    add     r6,r6,r1
+    add     r6,r6,r0
     ldr     r0,=#ADDR_KEY
     ldrh    r1,[r0]
     mov     r2,#VAL_KEY_LEFT
@@ -51,7 +50,7 @@ load_checkpoint:
     cmp     r2,#0x0
     beq     @store_checkpoint
     mov     r7,#0x24
-    add     r7,r3,r7
+    add     r7,r4,r7
     mov     r5,#0x0
     strh    r5,[r7]
     mov     r7,#0x80            ; Right held
@@ -63,8 +62,16 @@ load_checkpoint:
     lsl     r7,r7,1
     add     r6,r6,r7
 @store_checkpoint:
+    bl      @check_platinum
+    cmp     r0,#0x0
+    bne     @@not_platinum
+    mov     r0,#0xC0
+    lsl     r0,#0x2
+    add     r6,r6,r0
+@@not_platinum:
     ldrb    r5,[r6]
-    strb    r5,[r3,#0x2]
+@@really_store_checkpoint:
+    strb    r5,[r4,#0x2]
     ldr     r1,=#ADDR_STAGE_INDEX
     ldrb    r0,[r1]
     cmp     r0,#0x5             ; Phoenix reactor handling
@@ -75,14 +82,79 @@ load_checkpoint:
     ldrb    r0,[r2]
     strb    r0,[r1,#0xA]
 @checkpoints_end:
+    pop     {r4-r7}
     pop     r0
     bx      r0
     .pool
-    
-; phoenix reactors: 2->08  4->20  5->40  3->10
-    
+
+@check_platinum:
+    ldr     r0,=#ROMADDR_PLATINUM
+    ldr     r0,[r0]
+    ldr     r1,=#org(@plat_ascii)
+    ldr     r1,[r1]
+    cmp     r0,r1
+    beq     @@ret_0
+    mov     r0,#0x1
+    b       @@subr_end
+@@ret_0:
+    mov     r0,#0x0
+@@subr_end:
+    bx      r14
+    .pool
+@plat_ascii:
+    .ascii "PLAT"
+
 @phoenix_reactors:
     .db 0x0, 0x0, 0x8, 0x78, 0x28, 0x68, 0x0, 0x78, 0x78, 0x78, 0x78
+    .align 2
+
+    ; r0 = current checkpoint, r1 = stage index addr (0x0202EC30), r2 = current stage index
+@check_if_boss:
+    push    {r4-r7,r14}
+    mov     r4,r0
+    mov     r5,r1
+    bl      @check_platinum
+    cmp     r0,#0x0
+    bne     @@return_input
+    mov     r1,r5
+    ldr     r0,[r1,#0x30]
+    cmp     r0,#0x0
+    beq     @@return_input
+    mov     r3,#0xBE
+    lsl     r3,#0x1
+    add     r1,r1,r3
+    ldrb    r1,[r1]
+    ldr     r3,=#org(@boss_stage_states)
+    add     r3,r3,r2
+    ldrb    r0,[r3]
+    cmp     r1,r0
+    blt     @@check_if_kuwagust
+    ldr     r3,=#org(@boss_checkpoints)
+    add     r3,r3,r2
+    ldrb    r0,[r3]
+    b       @@subr_end
+@@check_if_kuwagust:
+    cmp     r2,#0x3
+    bne     @@return_input
+    cmp     r4,#0x3
+    bne     @@return_input
+    cmp     r1,#0x7
+    blt     @@return_input
+    mov     r0,#0x6
+    b       @@subr_end
+@@return_input:
+    mov     r0,r4
+@@subr_end:
+    pop     {r4-r7}
+    pop     r1
+    bx      r1
+    .pool
+
+@boss_stage_states:
+    .db 0xD, 0xA, 0x7, 0xD, 0x8, 0x8, 0x8, 0x3, 0x7, 0x3, 0xF, 0xF, 0x5, 0x5, 0x4, 0xE
+@boss_checkpoints:
+    .db 0x4, 0x4, 0x4, 0x8, 0x8, 0x4, 0x4, 0x3, 0x4, 0x3, 0x2, 0x2, 0x3, 0x3, 0x3, 0xC
+
     .endarea
     
     ; Current checkpoint
@@ -297,43 +369,43 @@ load_checkpoint:
     ; Current checkpoint
 
     ; Intro
-    .db 0,1,2,3
+    .db 0,1,2,3,4,4,4,4
     .align 0x10
     
     ; Hyleg
-    .db 0,1,2,3
+    .db 0,1,2,3,4,4
     .align 0x10
     
     ; Poler
-    .db 0,1,2,3
+    .db 0,1,2,3,4
     .align 0x10
     
     ; Kuwagust
-    .db 0,1,2,3,4,5
+    .db 0,1,2,3,4,5,6,6,8
     .align 0x10
     
     ; Phoenix
-    .db 0,1,2,3,4,5,6
+    .db 0,1,2,3,4,5,6,7,8,9,0xA
     .align 0x10
     
     ; Panter
-    .db 0,1,2,3
+    .db 0,1,2,3,4
     .align 0x10
     
     ; Burble
-    .db 0,1,2,3
+    .db 0,1,2,3,4,4
     .align 0x10
     
     ; Leviathan
-    .db 0,1,2
+    .db 0,1,2,3,3
     .align 0x10
     
     ; Harpuia
-    .db 0,1,2,3
+    .db 0,1,2,3,4,4
     .align 0x10
     
     ; Fefnir
-    .db 0,1,2
+    .db 0,1,2,3,3
     .align 0x10
     
     ; NA2
@@ -341,23 +413,23 @@ load_checkpoint:
     .align 0x10
     
     ; NA1
-    .db 0,1,2
+    .db 0,1,2,2
     .align 0x10
     
     ; Fefnir AP
-    .db 0,1,2
+    .db 0,1,2,3,3
     .align 0x10
     
     ; Leviathan AP
-    .db 0,1,2
+    .db 0,1,2,3,3
     .align 0x10
     
     ; Harpuia AP
-    .db 0,1,2
+    .db 0,1,2,3,3
     .align 0x10
     
     ; Final
-    .db 0,1,2,3,4,5
+    .db 0,1,2,3,4,5,1,1,1,3,3,3,0xC,0xC
     .align 0x10
     
     
@@ -365,43 +437,43 @@ load_checkpoint:
     ; Previous checkpoint
     
     ; Intro
-    .db 0,1,1,2
+    .db 0,1,1,2,3,3,3,3,3
     .align 0x10
     
     ; Hyleg
-    .db 0,1,1,2
-    .align 0x10
-    
-    ; Poler
-    .db 0,1,1,2
-    .align 0x10
-    
-    ; Kuwagust
     .db 0,1,1,2,3,4
     .align 0x10
     
+    ; Poler
+    .db 0,1,1,2,3
+    .align 0x10
+    
+    ; Kuwagust
+    .db 0,1,1,2,3,4,3,6,5
+    .align 0x10
+    
     ; Phoenix
-    .db 0,1,1,5,2,4,6
+    .db 0,1,1,5,2,4,6,2,3,4,5
     .align 0x10
     
     ; Panter
-    .db 0,1,1,2
+    .db 0,1,1,2,3
     .align 0x10
     
     ; Burble
-    .db 0,1,1,2
+    .db 0,1,1,2,3,4
     .align 0x10
     
     ; Leviathan
-    .db 0,1,1
+    .db 0,1,1,2,3
     .align 0x10
     
     ; Harpuia
-    .db 0,1,1,2
+    .db 0,1,1,2,3,4
     .align 0x10
     
     ; Fefnir
-    .db 0,1,1
+    .db 0,1,1,2,3
     .align 0x10
     
     ; NA2
@@ -409,23 +481,23 @@ load_checkpoint:
     .align 0x10
     
     ; NA1
-    .db 0,1,1
+    .db 0,1,1,2
     .align 0x10
     
     ; Fefnir AP
-    .db 0,1,1
+    .db 0,1,1,2,3
     .align 0x10
     
     ; Leviathan AP
-    .db 0,1,1
+    .db 0,1,1,2,3
     .align 0x10
     
     ; Harpuia AP
-    .db 0,1,1
+    .db 0,1,1,2,3
     .align 0x10
     
     ; Final
-    .db 0,0,1,2,3,4
+    .db 0,0,1,2,3,4,1,1,1,3,3,3,5,0xC
     .align 0x10
     
 
@@ -433,43 +505,43 @@ load_checkpoint:
     ; Next checkpoint
     
     ; Intro
-    .db 0,2,3,3
+    .db 0,2,3,4,4,4,4,4
     .align 0x10
     
     ; Hyleg
-    .db 0,2,3,3
+    .db 0,2,3,4,4,4
     .align 0x10
     
     ; Poler
-    .db 0,2,3,3
+    .db 0,2,3,4,4
     .align 0x10
     
     ; Kuwagust
-    .db 0,2,3,4,5,5
+    .db 0,2,3,6,5,8,7,4,8
     .align 0x10
     
     ; Phoenix
-    .db 0,2,4,3,5,3,6
+    .db 0,2,4,8,5,3,6,7,8,9,0xA
     .align 0x10
     
     ; Panter
-    .db 0,2,3,3
+    .db 0,2,3,4,4
     .align 0x10
     
     ; Burble
-    .db 0,2,3,3
+    .db 0,2,3,4,4,4
     .align 0x10
     
     ; Leviathan
-    .db 0,2,2
+    .db 0,2,3,3,3
     .align 0x10
     
     ; Harpuia
-    .db 0,2,3,3
+    .db 0,2,3,4,4,4
     .align 0x10
     
     ; Fefnir
-    .db 0,2,2
+    .db 0,2,3,3,3
     .align 0x10
     
     ; NA2
@@ -477,21 +549,21 @@ load_checkpoint:
     .align 0x10
     
     ; NA1
-    .db 0,2,2
+    .db 0,2,2,2
     .align 0x10
     
     ; Fefnir AP
-    .db 0,2,2
+    .db 0,2,3,3,3
     .align 0x10
     
     ; Leviathan AP
-    .db 0,2,2
+    .db 0,2,3,3,3
     .align 0x10
     
     ; Harpuia AP
-    .db 0,2,2
+    .db 0,2,3,3,3
     .align 0x10
     
     ; Final
-    .db 1,2,3,4,5,5
+    .db 1,2,3,4,5,0xC,2,2,2,4,4,4,0xD,0xD
     .endarea
